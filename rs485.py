fc45228f43c475da5ffb4218b9f097b1f764af5f
Nbinn <nhantt2001@gmail.com>
2024-03-28 19:51:44 +0700

    commit

---
+++ rs485.py
@@ -0,0 +1,106 @@
+print("Sensors and Actuators")
+
+import time
+import serial.tools.list_ports
+import paho.mqtt.client as mqtt
+
+
+
+MQTT_SERVER = "mqtt.ohstem.vn"
+MQTT_PORT = 1883
+MQTT_USERNAME = "testing12345"
+MQTT_PASSWORD = ""
+MQTT_TOPIC_PUB_TEMP = MQTT_USERNAME + "/temp"
+MQTT_TOPIC_PUB_MOIS = MQTT_USERNAME + "/mois"
+MQTT_TOPIC_SUB = MQTT_USERNAME + "/control"
+
+def mqtt_connected(client, userdata, flags, rc):
+    print("Connected succesfully!!")
+    client.subscribe(MQTT_TOPIC_SUB)
+
+def mqtt_subscribed(client, userdata, mid, granted_qos):
+    print("Subscribed to Topic!!!")
+
+def mqtt_recv_message(client, userdata, message):
+    print("Received: ", message.payload.decode("utf-8"))
+
+mqttClient = mqtt.Client()
+mqttClient.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+mqttClient.connect(MQTT_SERVER, int(MQTT_PORT), 60)
+
+mqttClient.on_connect = mqtt_connected
+mqttClient.on_subscribe = mqtt_subscribed
+mqttClient.on_message = mqtt_recv_message
+
+mqttClient.loop_start()
+
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+    return commPort
+    # return "/dev/ttyUSB1"
+
+portName = getPort()
+print(portName)
+
+
+
+try:
+    ser = serial.Serial(port=portName, baudrate=115200)
+    print("Open successfully")
+except:
+    print("Can not open the port")
+
+relay1_ON  = [2, 6, 0, 0, 0, 255, 201, 185]
+relay1_OFF = [2, 6, 0, 0, 0, 0, 137, 249]
+
+def setDevice1(state):
+    if state == True:
+        ser.write(relay1_ON)
+    else:
+        ser.write(relay1_OFF)
+    time.sleep(1)
+    print(serial_read_data(ser))
+
+def serial_read_data(ser):
+    bytesToRead = ser.inWaiting()
+    if bytesToRead > 0:
+        out = ser.read(bytesToRead)
+        data_array = [b for b in out]
+        print(data_array)
+        if len(data_array) >= 7:
+            array_size = len(data_array)
+            value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
+            return value
+        else:
+            return -1
+    return 0
+
+soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
+def readTemperature():
+    serial_read_data(ser)
+    ser.write(soil_temperature)
+    time.sleep(1)
+    return serial_read_data(ser)
+
+soil_moisture = [1, 3, 0, 7, 0, 1, 53, 203]
+def readMoisture():
+    serial_read_data(ser)
+    ser.write(soil_moisture)
+    time.sleep(1)
+    return serial_read_data(ser)
+
+
+while True:
+    print("TEST SENSOR")
+    mqttClient.publish(MQTT_TOPIC_PUB_TEMP,readTemperature())
+    time.sleep(1)
+    mqttClient.publish(MQTT_TOPIC_PUB_MOIS,readMoisture())
+    time.sleep(1)
